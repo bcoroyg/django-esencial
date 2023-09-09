88f786f53df56f79779b8916decb1ac98fe73b5a
Emilio Coroy <becgonzalez25@gmail.com>
2023-09-09 16:24:25 -0600

    MODULO 3: Creación del modelo de posts

--- socialgram/socialgram/urls.py
+++ socialgram/socialgram/urls.py
@@ -6,8 +6,10 @@ es más común delegar algo del mapeo a las propias aplicaciones.
 """
 
 # DJANGO
-from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
 
 from socialgram import views as local_views
 from posts import views as posts_views
@@ -18,5 +20,5 @@ urlpatterns = [
     path('hello-world/', local_views.hello_world),
     path('sorted/', local_views.sort_num),
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
-    path('posts/', posts_views.list_posts)
-]
+    path('posts/', posts_views.list_posts),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
