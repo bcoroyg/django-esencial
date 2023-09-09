2fd4fa45788952f671211866b5620577af9fd60c
Emilio Coroy <becgonzalez25@gmail.com>
2023-09-09 14:07:13 -0600

    MODULO 3: Extendiendo el modelo de usuario

--- socialgram/socialgram/urls.py
+++ socialgram/socialgram/urls.py
@@ -5,13 +5,16 @@ Define los mapeos url-vistas. A pesar de que éste podría contener todo el cód
 es más común delegar algo del mapeo a las propias aplicaciones.
 """
 
-
+# DJANGO
 from django.urls import path
+from django.contrib import admin
+
 from socialgram import views as local_views
 from posts import views as posts_views
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('hello-world/', local_views.hello_world),
     path('sorted/', local_views.sort_num),
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
