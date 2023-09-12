23ecf9899b96d3fa509cf32f1e7fdaa70b5314ae
Emilio Coroy <becgonzalez25@gmail.com>
2023-09-12 17:39:40 -0600

    MODULO 4: Logout

--- socialgram/users/views.py
+++ socialgram/users/views.py
@@ -1,7 +1,8 @@
 """ Users views """
 
 # DJANGO
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -22,3 +23,9 @@ def login_view(request):
                           {'error': 'Invalid username or password'}
                     )   
     return render(request, 'users/login.html')
+
+@login_required
+def logout_view(request):
+    """ Logout a user """
+    logout(request)
+    return redirect('login')
