1a95b9160b673012d40ffbac71cfff94cced42c8
Emilio Coroy <becgonzalez25@gmail.com>
2023-09-12 16:22:10 -0600

    MODULO 4: Templates y archivos estaticos

--- socialgram/posts/views.py
+++ socialgram/posts/views.py
@@ -123,4 +123,4 @@ def list_posts(request):
             <figure><img src="{picture}"/></figure>
         '''.format(**post))
     return HttpResponse('<br>'.join(content)) """
-    return render(request, 'feed.html', {'posts':posts})
+    return render(request, 'posts/feed.html', {'posts':posts})
