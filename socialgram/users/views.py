a4e4c2dd91dac7a0b69b05174f53dcf105394903
Emilio Coroy <becgonzalez25@gmail.com>
2023-09-13 10:17:36 -0600

    MODULO 4: Sign Up

--- socialgram/users/views.py
+++ socialgram/users/views.py
@@ -5,6 +5,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+# Exception
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
 # Create your views here.
 def login_view(request):
     """ Login view """
@@ -23,6 +30,34 @@ def login_view(request):
                           {'error': 'Invalid username or password'}
                     )   
     return render(request, 'users/login.html')
+
+def signup_view(request):
+    """ Sign up view. """
+    # import pdb; pdb.set_trace()
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
 
 @login_required
 def logout_view(request):
